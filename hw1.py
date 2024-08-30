aa6bebde1a763ea2ef155b6ee4ad0abc1592424d
Tsipiammar <tsipiammar@gmail.com>
2024-08-30 14:37:08 +0300

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,15 @@
+#question 1:
+#start
+x: int= int(input("enter a number?"));
+print(x);
+print(x);
+print(x);
+#end
+
+#question 2:
+x: int= int(777);
+y: int= int(888);
+z: int= int(999);
+
+result: int= x*y+z
+print(result);
